8336fcfb5769d239613f85cf6f86fd327d7bae26
Rusty Pickle <rusty.pickle94@gmail.com>
2026-05-24 19:14:16 +0600

    TWIM + who am I verification

--- driver-magno/src/main.rs
+++ driver-magno/src/main.rs
@@ -9,38 +9,42 @@ use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 const TWIM0: usize = 0x40003000;
-const PSEL_SCL_OFFSET: usize = 0x508;
-const PSEL_SDA_OFFSET: usize = 0x50C;
-const ADDRESS: usize = 0x588;
-const ENABLE: usize = 0x500;
 
-const FREQUENCY: usize = 0x524;
+const PSEL_SCL: *mut u32 = (0x508 + TWIM0) as *mut u32;
+const PSEL_SDA: *mut u32 = (0x50C + TWIM0) as *mut u32;
+
+const ADDRESS: *mut u32 = (0x588 + TWIM0) as *mut u32;
+const ENABLE: *mut u32 = (0x500 + TWIM0) as *mut u32;
+
+const FREQUENCY: *mut u32 = (0x524 + TWIM0) as *mut u32;
 const FREQUENCY_K100: u32 = 0x01980000;
 
 const MAGNO_SLAVE_ADDRESS: u32 = 0x1E;
 
 const GPIO0: usize = 0x50000000;
-const P8_OFFSET: usize = 0x720;
-const P16_OFFSET: usize = 0x740;
+const P8: *mut u32 = (0x720 + GPIO0) as *mut u32;
+const P16: *mut u32 = (0x740 + GPIO0) as *mut u32;
 
 const WHO_AM_I_M: usize = 0x4F;
 
-const RXD_PTR: usize = 0x534;
-const RXD_MAXCNT: usize = 0x538;
+const RXD_PTR: *mut u32 = (0x534 + TWIM0) as *mut u32;
+const TXD_PTR: *mut u32 = (0x544 + TWIM0) as *mut u32;
 
-const TXD_PTR: usize = 0x544;
-const TXD_MAXCNT: usize = 0x548;
+const RXD_MAXCNT: *mut u32 = (0x538 + TWIM0) as *mut u32;
+const TXD_MAXCNT: *mut u32 = (0x548 + TWIM0) as *mut u32;
 
-const TASKS_STARTTX: usize = 0x008;
-const TASKS_STARTRX: usize = 0x000;
+const TASKS_STARTTX: *mut u32 = (0x008 + TWIM0) as *mut u32;
+const TASKS_STARTRX: *mut u32 = (0x000 + TWIM0) as *mut u32;
 
-const EVENTS_LASTTX: usize = 0x11C;
-const EVENTS_LASTRX: usize = 0x15C;
+const EVENTS_LASTTX: *mut u32 = (0x11C + TWIM0) as *mut u32;
+const EVENTS_LASTRX: *mut u32 = (0x15C + TWIM0) as *mut u32;
 
-const EVENTS_ERROR: usize = 0x124;
+const SHORTS: *mut u32 = (0x200 + TWIM0) as *mut u32;
 
-const SHORTS_OFFSET: usize = 0x200;
-const ERROR_SRC: usize = 0x4C4;
+const EVENTS_ERROR: *mut u32 = (0x124 + TWIM0) as *mut u32;
+const ERROR_SRC: *mut u32 = (0x4C4 + TWIM0) as *mut u32;
+
+const EVENTS_STOPPED: *mut u32 = (0x104 + TWIM0) as *mut u32;
 
 #[derive(Default)]
 pub struct MagnoSensor {}
@@ -55,114 +59,104 @@ impl MagnoSensor {
         let scl_value = (connect << 31) | (port << 5) | scl_pin;
         let sda_value = (connect << 31) | (port << 5) | sda_pin;
         let pin_cnf_value = (3 << 2) | (6 << 8);
+
+        unsafe {
+            ptr::write_volatile(EVENTS_LASTTX, 0);
+            ptr::write_volatile(EVENTS_LASTRX, 0);
+            ptr::write_volatile(EVENTS_STOPPED, 0);
+
+            ptr::write_volatile(P8, pin_cnf_value);
+            ptr::write_volatile(P16, pin_cnf_value);
+
+            ptr::write_volatile(PSEL_SCL, scl_value);
+            ptr::write_volatile(PSEL_SDA, sda_value);
+
+            ptr::write_volatile(FREQUENCY, FREQUENCY_K100);
+
+            ptr::write_volatile(ADDRESS, MAGNO_SLAVE_ADDRESS);
+
+            ptr::write_volatile(ENABLE, 6);
+        }
+
+        let sensor = Self {};
+
+        sensor.clear_events();
+        let value = sensor.verify_who_am_i();
+
+        match value {
+            Ok(value) => {
+                if value != 64 {
+                    panic!("Wrong WHO_AM_I value: 0x{:X}", value);
+                }
+            }
+            Err(error) => {
+                panic!("Error: 0x{:X}", error);
+            }
+        }
+
+        sensor
+    }
+
+    fn verify_who_am_i(&self) -> Result<u8, u32> {
         let shorts_value: u32 = (1 << 7) | (1 << 12);
 
-        let psel_scl = (TWIM0 + PSEL_SCL_OFFSET) as *mut u32;
-        let psel_sda = (TWIM0 + PSEL_SDA_OFFSET) as *mut u32;
-
-        let frequency = (TWIM0 + FREQUENCY) as *mut u32;
-        let address = (TWIM0 + ADDRESS) as *mut u32;
-        let enable = (TWIM0 + ENABLE) as *mut u32;
-        let pin8 = (GPIO0 + P8_OFFSET) as *mut u32;
-        let pin16 = (GPIO0 + P16_OFFSET) as *mut u32;
-
-        let txd_ptr = (TWIM0 + TXD_PTR) as *mut u32;
-        let rxd_ptr = (TWIM0 + RXD_PTR) as *mut u32;
-
-        let txd_maxcnt = (TWIM0 + TXD_MAXCNT) as *mut u32;
-        let rxd_maxcnt = (TWIM0 + RXD_MAXCNT) as *mut u32;
-
         let tx_buf = [WHO_AM_I_M];
-
-        let tasks_starttx = (TWIM0 + TASKS_STARTTX) as *mut u32;
-        let tasks_startrx = (TWIM0 + TASKS_STARTRX) as *mut u32;
-
-        let events_lasttx = (TWIM0 + EVENTS_LASTTX) as *mut u32;
-        let events_error = (TWIM0 + EVENTS_ERROR) as *mut u32;
-        let events_lastrx = (TWIM0 + EVENTS_LASTRX) as *mut u32;
-
-        let error_src = (TWIM0 + ERROR_SRC) as *mut u32;
-        let shorts = (TWIM0 + SHORTS_OFFSET) as *mut u32;
-
         let mut rx_buf = [0u8; 1];
 
         unsafe {
-            ptr::write_volatile(shorts, shorts_value);
-            ptr::write_volatile(pin8, pin_cnf_value);
-            ptr::write_volatile(pin16, pin_cnf_value);
+            ptr::write_volatile(TXD_PTR, tx_buf.as_ptr() as u32);
+            ptr::write_volatile(RXD_PTR, rx_buf.as_mut_ptr() as u32);
 
-            ptr::write_volatile(psel_scl, scl_value);
-            ptr::write_volatile(psel_sda, sda_value);
+            ptr::write_volatile(TXD_MAXCNT, 1);
+            ptr::write_volatile(RXD_MAXCNT, 1);
 
-            ptr::write_volatile(frequency, FREQUENCY_K100);
+            ptr::write_volatile(SHORTS, shorts_value);
+            ptr::write_volatile(TASKS_STARTTX, 1);
+        }
+        loop {
+            let current_value = unsafe { ptr::read_volatile(EVENTS_STOPPED) };
 
-            ptr::write_volatile(address, MAGNO_SLAVE_ADDRESS);
-
-            ptr::write_volatile(enable, 6);
-
-            ptr::write_volatile(txd_ptr, tx_buf.as_ptr() as u32);
-
-            ptr::write_volatile(txd_maxcnt, 1);
-
-            ptr::write_volatile(tasks_starttx, 1);
-
-            loop {
-                let current_value = ptr::read_volatile(events_lasttx);
-
-                if current_value == 1 {
-                    ptr::write_volatile(events_lasttx, 0);
-                    ptr::write_volatile(rxd_maxcnt, 1);
-                    ptr::write_volatile(rxd_ptr, rx_buf.as_mut_ptr() as u32);
-                    ptr::write_volatile(tasks_startrx, 1);
-                    break;
-                }
-
-                let tx_error = ptr::read_volatile(events_error);
-
-                if tx_error == 1 {
-                    rprintln!("I2C Error Detected!");
-                    ptr::write_volatile(events_error, 0);
-                    break;
-                }
-
-                let error_src_val = ptr::read_volatile(error_src);
-
-                if error_src_val == 1 {
-                    rprintln!("I2C Error Source Detected!");
-                    ptr::write_volatile(error_src, 0);
-                    break;
-                }
+            if current_value == 1 {
+                break;
             }
 
-            loop {
-                let current_value = ptr::read_volatile(events_lastrx);
-
-                if current_value == 1 {
-                    ptr::write_volatile(events_lastrx, 0);
-                    break;
-                }
-
-                let tx_error = ptr::read_volatile(events_error);
-
-                if tx_error == 1 {
-                    rprintln!("I2C Error Detected!");
-                    ptr::write_volatile(events_error, 0);
-                    break;
-                }
-
-                let error_src_val = ptr::read_volatile(error_src);
-
-                if error_src_val == 1 {
-                    rprintln!("I2C Error Source Detected!");
-                    ptr::write_volatile(error_src, 0);
-                    break;
-                }
-            }
-
-            rprintln!("{:?}", rx_buf);
+            if let Some(error) = self.check_errors() {
+                return Err(error);
+            };
         }
 
-        Self {}
+        Ok(rx_buf[0])
+    }
+
+    fn check_errors(&self) -> Option<u32> {
+        unsafe {
+            let tx_error = ptr::read_volatile(EVENTS_ERROR);
+
+            if tx_error == 1 {
+                ptr::write_volatile(EVENTS_ERROR, 0);
+
+                let error_src_val = ptr::read_volatile(ERROR_SRC);
+
+                if error_src_val != 0 {
+                    ptr::write_volatile(ERROR_SRC, 0);
+                }
+
+                return Some(error_src_val);
+            }
+        }
+
+        None
+    }
+
+    fn clear_events(&self) {
+        unsafe {
+            ptr::write_volatile(EVENTS_LASTTX, 0);
+            ptr::write_volatile(EVENTS_LASTRX, 0);
+            ptr::write_volatile(EVENTS_STOPPED, 0);
+
+            ptr::write_volatile(EVENTS_ERROR, 0);
+            ptr::write_volatile(ERROR_SRC, 0);
+        }
     }
 }
 
@@ -171,6 +165,7 @@ fn main() -> ! {
     rtt_init_print!();
 
     let _ = MagnoSensor::new();
+    rprintln!("Magno sensor initialized");
     loop {
         wfi()
     }
